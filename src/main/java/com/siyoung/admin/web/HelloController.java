e58292a336795d94a9ac97644fbbcf70ca5b8c2c
KIMSIYOUNG <tldud2404@gmail.com>
2020-01-08 00:29:02 +0900

    feat : Add CRUD method to make a board  with jpa

--- src/main/java/com/siyoung/admin/web/HelloController.java
+++ src/main/java/com/siyoung/admin/web/HelloController.java
@@ -1,10 +1,12 @@
 package com.siyoung.admin.web;
 
 import com.siyoung.admin.web.dto.HelloResponseDto;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequiredArgsConstructor
 @RestController
 public class HelloController {
 
